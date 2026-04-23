db3c6278144081125d96a774eb935bd3a5615b08
hugotig8-wq <hugotig8@gmail.com>
2026-04-23 12:28:24 +0200

    Update API request body to include pricing and streaming

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -239,7 +239,7 @@ const manejarClickSeguro = async (producto, loader) => {
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body:JSON.stringify({"model": "Amazon.nova-lit-2-v1:0", "prompt": "Convenceme de renovar con: "+ producto.empresa +" u ofreceme una alternativa lógica.", "stream": false})
+        body:JSON.stringify({"model": "Amazon.nova-lit-2-v1:0", "prompt": "Convenceme de renovar con: "+ producto.empresa +" u ofreceme una alternativa lógica. Actualmente por mes pago: " + producto.precio, "stream": true})
       }
     );
 
