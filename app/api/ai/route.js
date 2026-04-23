94baaa0855be780d561f6c7a5fe9ea83295d2ea4
hugotig8-wq <hugotig8@gmail.com>
2026-04-23 10:55:09 +0200

    Refactor Bedrock client usage in route.js

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -1,14 +1,14 @@
 
 import { NextResponse } from 'next/server';
-import { BedrockAgentRuntimeClient, RetrieveAndGenerateCommand } from "@aws-sdk/client-bedrock-agent-runtime";
+
 import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
 
 
 export const maxDuration = 10000;
 
-async function preguntarAlDocumento(prompt, knowledgeBaseId) {
+/*async function preguntarAlDocumento(prompt, knowledgeBaseId) {
   try {
-    const client = new BedrockAgentRuntimeClient(
+    const client = new BedrockRuntimeClient(
       {region: "us-east-1", credentials: {
          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -35,24 +35,30 @@ async function preguntarAlDocumento(prompt, knowledgeBaseId) {
       console.error("Error al consultar Bedrock:", error);
       return NextResponse.json({ error:error.message }, { status: 500 });
    }
-}
+}*/
 
 export async function POST(request) {
   try{
     const {prompt, model, stream} = await request.json();
-    
-    return preguntarAlDocumento("Saber si no pagaron la retroactividad de los primeros meses de 2024.", process.env.BEDROCK_KNOWLEDGE_BASE_ID);
+    const client = new BedrockRuntimeClient(
+      {region: "us-east-1", credentials: {
+         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },});
+
+    const credentials = await client.config.credentials();
+    console.log('Credentials loaded:', !!credentials.accessKeyId);
+
+    /*  return preguntarAlDocumento("Saber si no pagaron la retroactividad de los primeros meses de 2024.", process.env.BEDROCK_KNOWLEDGE_BASE_ID);
   }catch(error){
     console.log("DETALLE ERROR 500: ",error);
     return NextResponse.json({ error:error.message }, { status: 500 });
-  }
-
-
+  }*/
+    const res = await client.send(new InvokeModelCommand({ 
     
-   /* const res = await client.send(new InvokeModelCommand({ 
-        modelId: "us.anthropic.claude-haiku-4-5-20251001-v1:0", 
+        modelId:"amazon.nova-2-lite-v1:0", 
         body: JSON.stringify({ 
-            anthropic_version: "bedrock-2023-05-31", 
+            amazon_version: "bedrock-2025-12-02", 
             max_tokens: 1024, 
             messages: [{ 
                 role: "user", 
@@ -61,20 +67,19 @@ export async function POST(request) {
         }) 
     })); 
 
-    if(!res.ok) {
+   /* if(!res.ok) {
             const errorData = await res.text();
             console.error("Error de Bedrock:", errorData);
             return NextResponse.json({ error: "Bedrock tardó demasiado o falló" }, { status: res.status });
-    }
+    }*/
+    console.log("Respuesta:", response.output.text);
+      
+    return NextResponse.json({ response: response.output.text });
 
-    const result = JSON.parse(new TextDecoder().decode(res.body)); 
-    setTimeout(10000);
-    //const data = await res.json();
-    return NextResponse.json({ response: result.content[0].text });
 }catch(error){
     console.log("DETALLE ERROR 500: ",error);
     return NextResponse.json({ error:error.message }, { status: 500 });
-}*/
+}
 }
 /*https://geoportal.minetur.gob.es/VCTEL/vcne.do debe ser checkeada junto con dirección del cliente y lugares de interés
 y rutas de google maps usando su api.*/
