b51243eed9d69dea948794bc072e0f5ec7737399
hugotig8-wq <hugotig8@gmail.com>
2026-07-04 02:55:11 +0000

    Permite usar el archivo instrumentation.js (o .ts) para usar la función register y preparar herramientas de monitoreo, en este casola usamos como sembrador de semilla en la BD. register se ejecuta cada vez que algun archivo fuerza reinicio de compilación, en run dev sería cada cambio, en deployment o despliegue sólo al hacer next start. Es mala praxis para siembra de datos pero asegurandose de persistencia puede funcionar. Sirve para introducción a herramientas de monitoreo.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    instrumentationHook: true,
+  },
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
