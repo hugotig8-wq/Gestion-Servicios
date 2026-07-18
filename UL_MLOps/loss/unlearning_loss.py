63d8215cb25a53c6edbb116030da069be34c64bf
hugotig8-wq <hugotig8@gmail.com>
2026-07-18 16:21:32 +0200

    Create unlearning_loss.py

---
+++ UL_MLOps/loss/unlearning_loss.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn.functional as F
+
+from loss.base_loss import BaseLoss
+
+
+class UnlearningLoss(BaseLoss):
+
+    def __init__(
+        self,
+        alpha: float = 1.0,
+        beta: float = 1.0
+    ):
+
+        self.alpha = alpha
+        self.beta = beta
+
+    def compute(
+
+        self,
+
+        retain_outputs,
+        forget_outputs,
+
+        retain_labels,
+        forget_labels
+
+    ):
+
+        retain_loss = F.cross_entropy(
+
+            retain_outputs.view(
+                -1,
+                retain_outputs.size(-1)
+            ),
+
+            retain_labels.view(-1)
+
+        )
+
+        forget_loss = -F.cross_entropy(
+
+            forget_outputs.view(
+                -1,
+                forget_outputs.size(-1)
+            ),
+
+            forget_labels.view(-1)
+
+        )
+
+        total_loss = (
+
+            self.alpha * retain_loss
+
+            +
+
+            self.beta * forget_loss
+
+        )
+
+        return (
+
+            total_loss,
+
+            retain_loss,
+
+            forget_loss
+
+        )
