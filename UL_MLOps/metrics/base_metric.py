eed517868df0c626bfb5747fbb2ce2bd5cdb0a5e
hugotig8-wq <hugotig8@gmail.com>
2026-07-18 15:58:22 +0200

    Update base_metric.py

--- UL_MLOps/metrics/base_metric.py
+++ UL_MLOps/metrics/base_metric.py
@@ -5,6 +5,8 @@ class BaseMetric(ABC):
 
     @abstractmethod
     def compute(self, model, *args, **kwargs) -> float:
+    # *args, **kwargs se encarga de usar sólo lo necesario
+    #Si definimos compute con todas las metricas sobrarían en cada cual.
         """
         Calcula la métrica y devuelve un valor entre 0 y 1.
         """
