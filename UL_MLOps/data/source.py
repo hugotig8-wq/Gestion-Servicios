13065654cdd8538ed9c454155efa9728ac11b93f
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 01:33:43 +0200

    Create source.py

---
+++ UL_MLOps/data/source.py
@@ -0,0 +1,8 @@
+#mañana quizá quieras leer:
+#un JSON;
+#un CSV;
+#un dataset de Hugging Face;
+#documentos del BOE;
+#un conjunto de datos propio.
+#Toda esa lógica debería vivir en un archivo independiente, por ejemplo:
+# para hacer texts= load_dataset(...)
