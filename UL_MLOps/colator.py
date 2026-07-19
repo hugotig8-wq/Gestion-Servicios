3150517f12d86a8a416a86bf5a20fabb9d350ac7
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 01:30:28 +0200

    Delete UL_MLOps/colator.py

--- UL_MLOps/colator.py
+++
@@ -1,7 +0,0 @@
-#Su responsabilidad sería construir el batch.
-#Actualmente el DataLoader utiliza el collator por defecto de PyTorch.
-#Pero en los LLM es habitual usar un DataCollator personalizado que:
-#haga el padding dinámico;
-#prepare correctamente las labels;
-#optimice la memoria.
-#Más adelante probablemente querremos implementar uno.
