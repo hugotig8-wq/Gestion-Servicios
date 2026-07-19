9425eb7f0f074ea07cce7a37953c4da4f2a38560
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 01:13:23 +0200

    Create data_loader.py

---
+++ UL_MLOps/data/data_loader.py
@@ -0,0 +1,92 @@
+from torch.utils.data import DataLoader
+
+from data.tokenizer import build_tokenizer
+from data.dataset import UnlearningDataset
+from data.split import split_dataset
+
+
+def build_data_loaders(
+
+    texts: list[str],
+
+    batch_size: int = 8,
+
+    max_length: int = 256,
+
+    shuffle: bool = True
+
+):
+
+    tokenizer = build_tokenizer()
+
+    splits = split_dataset(texts)
+
+    retain_dataset = UnlearningDataset(
+
+        texts=splits["retain"],
+
+        tokenizer=tokenizer,
+
+        max_length=max_length
+
+    )
+
+    forget_dataset = UnlearningDataset(
+
+        texts=splits["forget"],
+
+        tokenizer=tokenizer,
+
+        max_length=max_length
+
+    )
+
+    validation_dataset = UnlearningDataset(
+
+        texts=splits["validation"],
+
+        tokenizer=tokenizer,
+
+        max_length=max_length
+
+    )
+
+    retain_loader = DataLoader(
+
+        retain_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=shuffle
+
+    )
+
+    forget_loader = DataLoader(
+
+        forget_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=shuffle
+
+    )
+
+    validation_loader = DataLoader(
+
+        validation_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=False
+
+    )
+
+    return (
+
+        retain_loader,
+
+        forget_loader,
+
+        validation_loader
+
+    )
