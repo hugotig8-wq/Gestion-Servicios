bc8ecd9e5dc5bf231a83b19a8f2cdf4753064166
hugotig8-wq <hugotig8@gmail.com>
2026-07-19 17:50:57 +0200

    Update tokenizer.py

--- UL_MLOps/data/tokenizer.py
+++ UL_MLOps/data/tokenizer.py
@@ -4,10 +4,10 @@ from transformers import AutoTokenizer
 MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
 
-def build_tokenizer():
+def build_tokenizer(model_name: str):
 
     tokenizer = AutoTokenizer.from_pretrained(
-        MODEL_NAME
+        model_name
     )
 
     if tokenizer.pad_token is None:
