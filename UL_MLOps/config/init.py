49831609d11dbd346049d563877c8657c631f764
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 04:30:01 +0200

    Update init.py

--- UL_MLOps/config/init.py
+++ UL_MLOps/config/init.py
@@ -1,2 +1,9 @@
 from .config import Config
-from .config import Experiment
+from .experiment import Experiment
+from .experiment_status import ExperimentStatus
+
+__all__ = [
+    "Config",
+    "Experiment",
+    "ExperimentStatus",
+]
