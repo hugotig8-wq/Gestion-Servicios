2b85d1dbe0b3ac592b9bedf1d3b2423d686db4b6
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 02:36:47 +0200

    Update dataset.py

--- UL_MLOps/data/dataset.py
+++ UL_MLOps/data/dataset.py
@@ -1,34 +1,63 @@
 from torch.utils.data import Dataset
+import hashlib
 
 
 class UnlearningDataset(Dataset):
 
     def __init__(
+
         self,
+
         texts: list[str],
+
         tokenizer,
+
         max_length: int = 256
+
     ):
+
         self.texts = texts
+
         self.tokenizer = tokenizer
+
         self.max_length = max_length
 
     def __len__(self):
+
         return len(self.texts)
 
-    def __getitem__(self, index):
+    def __getitem__(
+
+        self,
+
+        index
+
+    ):
 
         text = self.texts[index]
 
+        example_id = hashlib.sha256(
+
+            text.encode("utf-8")
+
+        ).hexdigest()
+
         encoding = self.tokenizer(
+
             text,
+
             truncation=True,
+
             padding="max_length",
+
             max_length=self.max_length,
+
             return_tensors="pt"
+
         )
 
         input_ids = encoding["input_ids"].squeeze(0)
+
         attention_mask = encoding["attention_mask"].squeeze(0)
 
         labels = input_ids.clone()
@@ -37,7 +66,7 @@ class UnlearningDataset(Dataset):
 
         return {
 
-            "id": index,
+            "id": example_id,
 
             "input_ids": input_ids,
 
