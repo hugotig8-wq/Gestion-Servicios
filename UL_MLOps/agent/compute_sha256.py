e6da78f618a4ddd28f6d3f3c79e953cd6c571400
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 19:27:19 +0200

    Update compute_sha256.py

--- UL_MLOps/agent/compute_sha256.py
+++ UL_MLOps/agent/compute_sha256.py
@@ -1,10 +1,7 @@
 import hashlib
 from pathlib import Path
 
-
-class CommputeSha256:
-
-    def compute(self, old_path: Path = None, new_path: Path):
+def compute_sha256(old_path: Path = None, new_path: Path)-> str:
     
         hasher = hashlib.sha256()
 
