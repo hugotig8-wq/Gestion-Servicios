b90839a05414cc4b2ccc1898c40fc7f2373fd4fb
hugotig8-wq <hugotig8@gmail.com>
2026-07-20 03:49:32 +0200

    Create experiment.py

---
+++ UL_MLOps/config/experiment.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+
+from datetime import datetime
+
+import uuid
+
+from config.config import Config
+
+
+@dataclass
+
+class Experiment:
+
+    config: Config
+
+    experiment_id: str = field(init=False)
+
+    started_at: datetime = field(init=False)
+
+    def __post_init__(self):
+
+        self.started_at = datetime.utcnow()
+
+        timestamp = self.started_at.strftime(
+
+            "%Y%m%d_%H%M%S"
+
+        )
+
+        random = uuid.uuid4().hex[:8]
+
+        self.experiment_id = f"{timestamp}_{random}"
