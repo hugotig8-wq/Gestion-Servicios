a01a5ce2afc5123d6ca12aa197af4d4be92fb16c
hugotig8-wq <hugotig8@gmail.com>
2026-08-01 15:06:28 +0200

    Add docstring for computeSha256 function
    Added docstring to computeSha256 function.

--- UL_MLOps/agent/compute_sha256.py
+++ UL_MLOps/agent/compute_sha256.py
@@ -2,7 +2,23 @@ import hashlib
 from pathlib import Path
 
 def computeSha256(old_path: Path = None, new_path: Path)-> str:
-    
+        """
+        Retorna el hash en chunks de 8192
+
+        Parámetros
+        ----------
+        old_path
+            Dirección antigua.
+
+        new_path
+            Dirección nueva.
+
+        Retorna
+        -------
+        Retorna un hasher de SHA256.
+
+
+        """
         hasher = hashlib.sha256()
 
         with open(
