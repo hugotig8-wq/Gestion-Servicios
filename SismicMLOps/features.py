b0eb4039bbd94f4c2f999bd59326d9ec9b01ca5d
hugotig8-wq <hugotig8@gmail.com>
2026-08-27 12:42:47 +0200

    Update import path for config module

--- SismicMLOps/features.py
+++ SismicMLOps/features.py
@@ -4,7 +4,7 @@ import math
 from typing import Optional
 import numpy as np
 import pandas as pd
-from SismicMLOps.config import (
+from config import (
     CELL_KM,
     GRID_COLS,
     GRID_MIN_LAT,
